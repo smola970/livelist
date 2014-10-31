125c87061afdce8d1078e6b75d1e1fdd29edf437
Lauren Smolenski <lauren@smolenski.net.nz>
2014-10-31 13:31:52 +1100

    changed seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ActiveRecord::Base
 
   validates :username, :uniqueness => true
   validates :username, :presence => true
-  # validates :username, :length => { :in => 5..30 :message => "Your username must be between five and thirty characters" }
-  validates :username, format: { without: /\s/ }
-  # validates :password_confirmation, :presence => true
+  # # validates :username, :length => { :in => 5..30 :message => "Your username must be between five and thirty characters" }
+  # validates :username, format: { without: /\s/ }
+  # # validates :password_confirmation, :presence => true
 
   mount_uploader :cover_image_url, ImageUploader
   mount_uploader :profile_image_url, ImageUploader
